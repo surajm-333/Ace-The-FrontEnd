7b4881993b3eea0131c4263f0efbec279306517b
surajm-333 <surajmulchandani37@gmail.com>
2021-11-04 12:58:46 +0530

    Added necessary script tags in html files and set up firebase.js

---
+++ Login Form using Firebase/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAc0gg3L3gwv9oN39UKeh56TToG9SJG7CE",
+    authDomain: "loginform-92a41.firebaseapp.com",
+    projectId: "loginform-92a41",
+    storageBucket: "loginform-92a41.appspot.com",
+    messagingSenderId: "315858221330",
+    appId: "1:315858221330:web:93369c8290feb706bf5bcb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
