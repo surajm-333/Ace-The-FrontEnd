cb967c991bef2ebf74836f97c9568c8c787ff739
surajm-333 <surajmulchandani37@gmail.com>
2021-11-04 15:04:31 +0530

    Implemented login, signup and forgot password functionalities

--- Login Form using Firebase/firebase.js
+++ Login Form using Firebase/firebase.js
@@ -9,5 +9,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
---
+++ Login Form using Firebase/index.js
@@ -0,0 +1,39 @@
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        location.replace("welcome.html");
+    }
+})
+
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch((error) => {
+            document.getElementById("error") = "Error " + error.code + ": " + error.message;
+        });
+}
+
+function signUp() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch((error) => {
+            document.getElementById("error") = "Error " + error.code + ": " + error.message;
+        });
+}
+
+function forgotPassword() {
+    const email = document.getElementById("email").value;
+    firebase.auth().sendPasswordResetEmail(email)
+        .then(() => {
+            alert("Password reset email sent!");
+        })
+        .catch((error) => {
+            document.getElementById("error") = "Error " + error.code + ": " + error.message;
+        });
+}
+
