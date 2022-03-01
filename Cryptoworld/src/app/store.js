d0520014fc213d7aef6c7216d80441014a323180
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-03-01 18:08:04 +0530

    Added Working Photos and File management of CryptoRush Application

--- Cryptoworld/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { cryptoApi } from '../services/cryptoApi'
-import { cryptoNewsApi } from '../services/cryptoNewsApi'
-export const store = configureStore({
-  reducer: {
-    [cryptoApi.reducerPath] : cryptoApi.reducer,
-    [cryptoNewsApi.reducerPath] : cryptoNewsApi.reducer
-  }
-})
