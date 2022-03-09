f0889c935c6d06f6bfe77763f2f9228c72a90d69
Luffy <71889838+Kalyani02072003@users.noreply.github.com>
2022-03-09 19:31:13 +0530

    changed fontawesome error

--- Profile-card-using-reactjs/src/components/Content.js
+++ Profile-card-using-reactjs/src/components/Content.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../style.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
 export default function Content() {
     return (
         <section>
