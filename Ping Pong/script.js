9da583a914935b4fca2f9ff2dadc3657100ff866
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-09 05:30:20 -0700

    :memo: js variable added

---
+++ Ping Pong/script.js
@@ -0,0 +1,9 @@
+var ball = document.getElementById('ball');
+var rod1 = document.getElementById('rod1');
+var rod2 = document.getElementById('rod2');
+
+
+const storeName = "PPName";
+const storeScore = "PPMaxScore";
+const rod1Name = "Rod 1";
+const rod2Name = "Rod 2";
