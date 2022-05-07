43854f3013c91a42876fff5629345b2c2a4aca89
praveen-221 <marveen223@gmail.com>
2022-05-07 20:39:53 +0530

    Added styles

--- Google UI Clone/index.js
+++ Google UI Clone/index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#form_submit');
 const search = document.querySelector('#search');
 
+//function to fetch the search query string and respond to the search and link to website
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   squery = search.value;
@@ -11,11 +12,11 @@ form.addEventListener('submit', (e) => {
   } else {
     window.location = `https://www.google.com/search?q=${squery}`;
     search.value = '';
-    console.log('Inside else');
+    console.log('Inside else'); 
   }
 });
 
-function signin() {
+function signin() { //function to go to signin page
   window.location =
     'https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAmgQ';
 }
