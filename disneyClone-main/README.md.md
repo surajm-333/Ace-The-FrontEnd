94cbb55623bff5225eee7fdc1721f0003f77b8f7
Shaon Dhar <shaonnw@gmail.com>
2022-05-27 19:01:57 +0530

    feat: disney hotstar clone added renew

---
+++ disneyClone-main/README.md.md
@@ -0,0 +1,27 @@
+﻿<h1>Disney Hotstar Clone </h1>
+
+<p>Landing page clone of Disney Hotstar</p>
+
+<h3>Used Technologies</h3>
+
+<ul>
+
+<li>HTML5</li>
+
+<li>CSS3</li>
+
+<li>JavaScript</li>
+
+</ul>
+
+### Steps to Use:
+
+
+**1.** Clone this Repository to your local machine.
+
+
+**2.** Navigate to the project directory:
+
+After cloning this to your local machine. Open the project folder. And here you can directly run the Index.html file.
+
+
---
+++ disneyClone-main/data.js
@@ -0,0 +1,32 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "https://i.ibb.co/ZXFxBXy/slider-2.png",
+  },
+  {
+    name: "Burrow",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "https://i.ibb.co/FWZp9Jb/Screenshot-2021-10-22-at-11-34-02-AM.png",
+  },
+  {
+    name: "loki",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "https://i.ibb.co/Wx9qMys/slider-1.png",
+  },
+  {
+    name: "wanda vision",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "https://i.ibb.co/KD0tVVc/slider-3.png",
+  },
+  {
+    name: "raya and the last dragon",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "https://i.ibb.co/kg8yG7y/slider-4.png",
+  },
+  {
+    name: "luca",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "https://i.ibb.co/M8RgzxN/slider-5.png",
+  },
+];
