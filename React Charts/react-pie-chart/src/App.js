3d77d807dd894982f22df31e4aabfee66ee7f02d
Mahesh Gaikwad <91421427+777ahesh@users.noreply.github.com>
2022-05-28 19:11:14 +0530

    added files

--- React Charts/react-pie-chart/src/App.js
+++ React Charts/react-pie-chart/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 import Chart from "react-apexcharts";
 
@@ -7,49 +7,50 @@ const options = {
   title: {
     text: "Current Downloads ",
     align: "left",
-    margin:50
+    margin: 50,
   },
-  series:[44, 55, 41, 17],
-  labels:["Mac", "Windows", "iOS", "Android"],
-  position:"bottom",
-  colors:["#C8FACD","#5BE584","#00AB55","#007B55"],
-  dataLabels:{
-    enabled:false
+  series: [44, 55, 41, 17],
+  labels: ["Mac", "Windows", "iOS", "Android"],
+  position: "bottom",
+  colors: ["#C8FACD", "#5BE584", "#00AB55", "#007B55"],
+  dataLabels: {
+    enabled: false,
   },
-  plotOptions:{
-    pie:{
-      expandOnClick:false,
-      donut:{
-        size:"80px",
-        labels:{
-        show:true,
-          total:{
-            show:true,
-            showAlways:false,
-            fontSize:"24px",
-            color:"#2787AB"
-          }
-        }
-      }
-    }
-  }
+  plotOptions: {
+    pie: {
+      expandOnClick: false,
+      donut: {
+        size: "80px",
+        labels: {
+          show: true,
+          total: {
+            show: true,
+            showAlways: false,
+            fontSize: "24px",
+            color: "#2787AB",
+          },
+        },
+      },
+    },
+  },
 };
-
 
 function App() {
   return (
-    <div className="installation">
-      <div className="installLeft">
-        <Chart   
-          options={options}
-          series={series}
-          type="donut"
-          height="100%"
-          width="100%"
-        />
+    <>
+      <h1 align="center">React Pie Chart</h1>
+      <div className="installation">
+        <div className="installLeft">
+          <Chart
+            options={options}
+            series={series}
+            type="donut"
+            height="100%"
+            width="100%"
+          />
+        </div>
       </div>
-    </div>
-    
+    </>
   );
 }
 
